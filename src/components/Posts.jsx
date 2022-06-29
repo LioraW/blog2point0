18bca6050f3ede786a2b0423e293cdd0cd529d4b
LioraW <lcwdmg@umsystem.edu>
2022-06-28 21:09:24 -0400

    refactored each post into its own componet, saving still has a bug'

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -13,19 +13,82 @@ import updatePost from '../api/update'
 import deletePost from '../api/deletePost';
 import '../App.css';
 
-
-
-
 const superagent = require('superagent')
-const buttonStyle = {
-    marginRight: '10px',
-    textDecoration: 'none',
-    margiLeft: 'auto'
+
+
+const PostCard = ( {id, post_title, post_text} ) => {
+    const [newTitle, setNewTitle] = useState("")
+    const [newText, setNewText] = useState("")
+    
+    const [beingEdited, setBeingEdited] = useState(false)
+
+    const handleTitleChange = event => { setNewTitle(event.target.value) }
+    const handleTextChange = event => { setNewText(event.target.value) }
+
+    const editPost = async (id) => {
+        await updatePost(id, { title: newTitle, text: newText })
+    }
+
+
+    return (
+        <>
+            <Paper elevation={10} style={{ background: lightBlue, padding: 20 }}>
+
+                {
+                    beingEdited ?
+                        <div>
+                            <input type="text" value={post_title} title="Enter a new title" placeholder={newTitle} onChange={handleTitleChange} />
+                            <input type="text" value={post_text} title="Enter new text" placeholder={newText} onChange={handleTextChange} />
+                        </div>
+                        :
+                        <div>
+                            <div className='post-title'>
+                                {post_title}
+                            </div>
+                            <div className='post-text'>
+                                {post_text}
+                            </div>
+                        </div>
+
+                }
+
+                <div className='edit-options'>
+                    {
+                        beingEdited ?
+                            <div>
+                                <IconButton aria-label="save"
+                                    onClick={async () => { editPost(id); setBeingEdited(false) }}>
+                                    <SaveIcon />
+                                </IconButton>
+                            </div>
+                            :
+                            //updated button had onclick like onClick={async () => { editPost(post._id); setBeingEdited(true) }
+                            <div>
+                                <IconButton aria-label="update"
+                                    onClick={async () => { setBeingEdited(true) }}>
+                                    <EditIcon />
+                                </IconButton>
+                            </div>
+
+                    }
+
+                    <IconButton aria-label="delete"
+                        onClick={async () => { await deletePost(id) }}>
+                        <DeleteIcon />
+                    </IconButton>
+
+                </div>
+            </Paper>
+        </>
+
+    )
 }
 
 const Posts = () => {
 
     const [posts, setPosts] = useState([])
+    const [beingEdited, setBeingEdited] = useState(false)
+
     const [newTitle, setNewTitle] = useState("")
     const [newText, setNewText] = useState("")
 
@@ -34,7 +97,7 @@ const Posts = () => {
     const handleTextChange = event => { setNewText(event.target.value) }
 
     const editPost = async (id) => {
-        await  updatePost(id, { title: newTitle, text: newText }) 
+        await updatePost(id, { title: newTitle, text: newText })
     }
 
 
@@ -50,6 +113,7 @@ const Posts = () => {
 
     useEffect(() => {
         getPosts()
+
     }, [posts])
 
     return (
@@ -71,26 +135,7 @@ const Posts = () => {
                             return (
                                 <tr key={post._id}>
                                     <td>
-                                        <Paper elevation={10} style={{ background: lightBlue, padding: 20 }}>
-                                            <div className='post-title'>
-                                                {post.title}
-                                            </div>
-                                            <div className='post-text'>
-                                                {post.text}
-                                            </div>
-                                            
-                                            <div className='edit-options'>
-                                                <IconButton aria-label="update"
-                                                    onClick={ async () => { editPost(post._id) } }>
-                                                    <EditIcon />
-                                                </IconButton>
-                                                <IconButton aria-label="delete"
-                                                    onClick={ async () => { await deletePost(post._id) }}>
-                                                    <DeleteIcon />
-                                                </IconButton>
-
-                                            </div>
-                                        </Paper>
+                                        <PostCard id={post._id} post_title={post.title} post_text={post.text} editPost={editPost}/>
                                     </td>
                                 </tr>
                             )
